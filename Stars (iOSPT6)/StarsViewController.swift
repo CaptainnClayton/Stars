6315145416b42c5d43dbcc9d908c5b126b1dd656
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-23 19:06:10 -0700

    Set up views and connections

---
+++ Stars (iOSPT6)/StarsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Stars (iOSPT6)
+//
+//  Created by Clayton Watkins on 4/23/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: IBActions
+    @IBAction func printButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func createButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: UITableViewDataSource
+
+}
+
+class StarTableViewCell: UITableViewCell {
+    // MARK: IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+}
