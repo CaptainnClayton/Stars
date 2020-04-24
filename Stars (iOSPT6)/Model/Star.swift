fa03a3c471e8bb31d7de26c014daa7f183b3fde3
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-23 20:26:33 -0700

    Added persistence to app

---
+++ Stars (iOSPT6)/Model/Star.swift
@@ -0,0 +1,17 @@
+//
+//  Star.swift
+//  Stars (iOSPT6)
+//
+//  Created by Clayton Watkins on 4/23/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import Foundation
+
+struct Star: Codable{
+    var name: String
+    var distance: Double
+}
+
+
+// MVC - Models, Views, Controllers
